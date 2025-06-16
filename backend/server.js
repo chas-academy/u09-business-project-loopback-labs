9e08e01a80d79c312490cd551b1502c5946b3f78
Joakim Helgodt <joakim.helgodt@gmail.com>
2025-06-16 14:12:14 +0200

    chore: downgrade eslint-plugin-import to version 2.29.1 and standardize quotes in server.js

--- backend/server.js
+++ backend/server.js
@@ -1,22 +1,22 @@
-require('dotenv').config();
-const express = require('express');
-const mongoose = require('mongoose');
-const cors = require('cors');
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
 
 const app = express();
 
 // Middleware
-const allowedOrigins = ['http://localhost:5173', 'https://mimir1.netlify.app'];
+const allowedOrigins = ["http://localhost:5173", "https://mimir1.netlify.app"];
 
 const corsOptions = {
   origin(origin, callback) {
-    if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+    if (!origin || allowedOrigins.includes(origin)) {
       callback(null, true);
     } else {
-      callback(new Error('Not allowed by CORS'));
+      callback(new Error("Not allowed by CORS"));
     }
   },
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true,
 };
 
@@ -27,26 +27,27 @@ app.use(express.json());
 mongoose
   .connect(process.env.MONGO_URI)
   // eslint-disable-next-line no-console
-  .then(() => console.log('Connected to MongoDB Atlas'))
-  .catch((err) => console.error('MongoDB Atlas connection error:', err));
+  .then(() => console.log("Connected to MongoDB Atlas"))
+  .catch((err) => console.error("MongoDB Atlas connection error:", err));
 
 // Routes
-const recipeRoutes = require('./routes/recipeRoutes');
-
-app.use('/api/recipes', recipeRoutes);
+const recipeRoutes = require("./routes/recipeRoutes");
+app.use("/api/recipes", recipeRoutes);
 
 // Basic route
-app.get('/', (req, res) => {
-  res.json({ message: 'Welcome to Mimir API' });
+app.get("/", (req, res) => {
+  res.json({ message: "Welcome to Mimir API" });
 });
 
 // Error handling middleware
-app.use((err, req, res) => {
+/* eslint-disable-next-line no-unused-vars */
+app.use((err, req, res, next) => {
   console.error(err.stack);
-  res.status(500).json({ message: 'Something went wrong!' });
+  res.status(500).json({ message: "Something went wrong!" });
 });
 
 const PORT = process.env.PORT || 5001;
 app.listen(PORT, () => {
-
+  // eslint-disable-next-line no-console
+  console.log(`Server running on port ${PORT}`);
 });
